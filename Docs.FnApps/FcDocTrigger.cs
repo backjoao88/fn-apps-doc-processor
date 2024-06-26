0eded52da3d2bebf6453c8fd6b83ff714cfc4a2d
Back <joaoback47@gmail.com>
2024-06-26 07:57:56 -0300

    add: queue as an output binding for blob trigger

--- Docs.FnApps/FcDocTrigger.cs
+++ Docs.FnApps/FcDocTrigger.cs
@@ -2,17 +2,22 @@
 using Doc.FnApps.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Doc.FnApps;
 
-public static class FcDocTrigger
+[StorageAccount("AzureWebJobsStorage")]
+public class FcDocTrigger
 {
     [FunctionName("FcDocTrigger")]
-    public static void Run([BlobTrigger("tobeprocess/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob,
+    [return: Queue("queueprocess")]
+    public string Run([BlobTrigger("tobeprocess/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob,
         string name, ILogger log)
     {
         var xmlReader = new IssuerXmlReader();
         var issuer = xmlReader.ReadEmit(myBlob);
         log.LogInformation($"Issuer name is {issuer.Name}; Issuer company is {issuer.Company}");
+        return JsonConvert.SerializeObject(issuer);
     }
 }
